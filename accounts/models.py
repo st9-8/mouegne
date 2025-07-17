16761e9d57e2424ad7a6f28e07ed76e7e05a7135
Stephane Fedim <stephanefedim@gmail.com>
2025-07-17 14:32:50 +0100

    Fix: displaying customer in admin

--- accounts/models.py
+++ accounts/models.py
@@ -78,7 +78,7 @@ class Customer(models.Model):
         db_table = 'Customers'
 
     def __str__(self) -> str:
-        return self.first_name + " " + self.last_name
+        return f'{self.pk}' + self.first_name or '' + " " + self.last_name or ''
 
     def get_full_name(self):
         return self.first_name or '' + " " + self.last_name or ''
