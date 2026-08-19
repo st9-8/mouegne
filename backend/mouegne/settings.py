f92cb0ed663c23ae9c016e0f57a973e57fc88328
Stephane Fedim <stephanefedim@gmail.com>
2026-08-19 10:08:14 +0100

    Update: local nginx

--- backend/mouegne/settings.py
+++ backend/mouegne/settings.py
@@ -165,6 +165,7 @@ LOGOUT_URL = 'logout'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')
                     ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
